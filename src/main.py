d2d35809d6d31e9650d1fa390c2e71d30c002685
elmiado <sayad.mahdi75@gmail.com>
2025-07-13 05:39:46 +0330

    making sure we use uvloop at top level

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from src.api.routes import subscription
-
+from contextlib import asynccontextmanager
 import asyncio
 import logging
 import uvloop
 
-app = FastAPI()
-
-app.include_router(subscription.router)
 LOGGER = logging.getLogger("uvicorn.error")
 
 
-@app.on_event("startup")
-async def check_loop():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_running_loop()
     LOGGER.info(f"Checking Uvloop: {type(loop) == uvloop.Loop}")
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(subscription.router)
