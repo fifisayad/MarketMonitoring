69a4a2aad07e15eecc633a07f01027aa959b2c72
elmiado <sayad.mahdi75@gmail.com>
2025-07-13 04:30:07 +0330

    check uvloop in python after enabling in uvcorn

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from src.api.routes import subscription
+from fifi.helpers.get_logger import GetLogger
+import asyncio
+import logging
 
 app = FastAPI()
 
 app.include_router(subscription.router)
+LOGGER = logging.getLogger("uvicorn.error")
+
+
+@app.on_event("startup")
+async def check_loop():
+    loop = asyncio.get_running_loop()
+    LOGGER.info(f"USING UVLOOP: {type(loop)}")
