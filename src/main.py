fe588840262acbe2973872787ff8d68f0af61c04
elmiado <sayad.mahdi75@gmail.com>
2025-07-13 04:34:25 +0330

    check loop in startuo stage

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from src.api.routes import subscription
-from fifi.helpers.get_logger import GetLogger
+
 import asyncio
 import logging
+import uvloop
 
 app = FastAPI()
 
@@ -13,4 +14,4 @@ LOGGER = logging.getLogger("uvicorn.error")
 @app.on_event("startup")
 async def check_loop():
     loop = asyncio.get_running_loop()
-    LOGGER.info(f"USING UVLOOP: {type(loop)}")
+    LOGGER.info(f"Checking Uvloop: {type(loop) == uvloop.Loop}")
