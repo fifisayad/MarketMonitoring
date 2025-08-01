232071f92733f23380ac167bb4c2b516c4b4b41f
fifisayad <sayad.mehrdad@gmail.com>
2025-08-01 15:28:36 +0330

    feat: add mainnet url setting

--- src/common/settings.py
+++ src/common/settings.py
@@ -1,5 +1,6 @@
+from hyperliquid.utils.constants import MAINNET_API_URL, TESTNET_API_URL
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    pass
+    HYPERLIQUID_BASE_URL: str = MAINNET_API_URL
