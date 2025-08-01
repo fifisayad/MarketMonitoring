702b11c44184511bff75b0e791b642bf5bc0c340
fifisayad <sayad.mehrdad@gmail.com>
2025-08-01 13:42:11 +0330

    feat: add exchanges extneded class

---
+++ src/service/exchanges/binance_exchange_worker.py
@@ -0,0 +1,23 @@
+from .base import BaseExchangeWorker
+from ...enums.exchange import Exchange
+from ...enums.market import Market
+from ...enums.data_type import DataType
+
+
+class BinanceExchangeWorker(BaseExchangeWorker):
+    exchange = Exchange.BINANCE
+
+    def __init__(self, market: Market):
+        super().__init__(market)
+
+    async def start(self):
+        return await super().start()
+
+    async def subscribe(self, data_type: DataType):
+        return await super().subscribe(data_type)
+
+    async def publish(self):
+        return await super().publish()
+
+    async def stop(self):
+        return await super().stop()
