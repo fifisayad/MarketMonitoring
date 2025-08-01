f5d9bb536f44967de7bd6252d981630ea946b7cc
fifisayad <sayad.mehrdad@gmail.com>
2025-08-01 15:35:37 +0330

    feat: add start method for hyperliquid info

--- src/service/exchanges/hyperliquid_exchange_worker.py
+++ src/service/exchanges/hyperliquid_exchange_worker.py
@@ -1,19 +1,28 @@
+from hyperliquid.info import Info
 from .base import BaseExchangeWorker
 from ...enums.exchange import Exchange
 from ...enums.market import Market
 from ...enums.data_type import DataType
+from ...common.settings import Settings
 
 
 class HyperliquidExchangeWorker(BaseExchangeWorker):
     exchange = Exchange.HYPERLIQUID
+    base_url: str
+    info: Info
 
     def __init__(self, market: Market):
         super().__init__(market)
+        self.settings = Settings()
+        self.base_url = self.settings.HYPERLIQUID_BASE_URL
 
     async def start(self):
-        return await super().start()
+        self.info = Info(self.base_url)
 
     async def subscribe(self, data_type: DataType):
+        if self.is_data_type_subscribed(data_type):
+            return
+
         return await super().subscribe(data_type)
 
     async def publish(self):
