7cbd9cccc9f4a49c704efdfecdb313c2acad0aed
fifisayad <sayad.mehrdad@gmail.com>
2025-08-01 13:00:27 +0330

    feat: add data type enum

--- src/common/schemas.py
+++ src/common/schemas.py
@@ -2,8 +2,10 @@ from pydantic import BaseModel
 
 from ..enums.exchange import Exchange
 from ..enums.market import Market
+from ..enums.data_type import DataType
 
 
 class SubscriptionRequestSchema(BaseModel):
     exchange: Exchange
     market: Market
+    data_type: DataType
