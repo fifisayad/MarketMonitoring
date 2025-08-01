743be32d4ea30a8402c5608891ccdbf8ac8045dd
fifisayad <sayad.mehrdad@gmail.com>
2025-08-01 12:55:39 +0330

    refactor: remove unneccessary configs from setting

--- src/common/settings.py
+++ src/common/settings.py
@@ -2,13 +2,4 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_host: str = "127.0.0.1"
-    app_port: int = 8000
-    app_reload: bool = False
-    app_loop: str = "uvloop"
-
-    class Config:
-        env_file = ".env"
-
-
-settings = Settings()
+    pass
