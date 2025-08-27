67fd7615978083a24de6d842eede90add5271d41
elmiado <sayad.mahdi75@gmail.com>
2025-08-27 14:30:57 +0330

    refacort: add new susbcribe schema for indicators

--- src/common/schemas.py
+++ src/common/schemas.py
@@ -3,10 +3,12 @@ from pydantic import BaseModel, field_validator
 from ..enums.exchange import Exchange
 from ..enums.market import Market
 from ..enums.data_type import DataType
+from ..enums.indicator_type import IndicatorType
+
 from typing import List, Dict, Any
 
 
-class SubscriptionRequestSchema(BaseModel):
+class MarketSubscriptionRequestSchema(BaseModel):
     exchange: Exchange
     market: Market
     data_type: DataType
@@ -16,6 +18,12 @@ class SubscriptionRequestSchema(BaseModel):
         if v == DataType.INFO:
             raise ValueError("INFO is not allowed as a data_type")
         return v
+
+
+class IndicatorSubscriptionRequest(BaseModel):
+    exchange: str
+    market: str
+    indicator: IndicatorType
 
 
 class SubscriptionResponseSchema(BaseModel):
