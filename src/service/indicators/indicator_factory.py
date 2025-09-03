390f0ab716465c332f43c2e4969499efd32c29f3
elmiado <sayad.mahdi75@gmail.com>
2025-09-03 16:48:33 +0330

    feat: pass kwargs to indicators

--- src/service/indicators/indicator_factory.py
+++ src/service/indicators/indicator_factory.py
@@ -8,13 +8,16 @@ from .sma_indicator import SMAIndicator
 
 
 def create_indicator(
-    exchange: Exchange, market: Market, data_type: IndicatorType
+    exchange: Exchange,
+    market: Market,
+    data_type: IndicatorType,
+    **kwargs,
 ) -> BaseIndicator:
     if data_type == IndicatorType.RSI:
-        return RSIIndicator(exchange=exchange, market=market)
+        return RSIIndicator(exchange=exchange, market=market, **kwargs)
     elif data_type == IndicatorType.MACD:
-        return MACDIndicator(exchange=exchange, market=market)
+        return MACDIndicator(exchange=exchange, market=market, **kwargs)
     elif data_type == IndicatorType.SMA:
-        return SMAIndicator(exchange=exchange, market=market)
+        return SMAIndicator(exchange=exchange, market=market, **kwargs)
     else:
         raise ValueError(f"There isn't indicator for {data_type.value}")
