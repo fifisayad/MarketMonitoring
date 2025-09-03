2b1d6668ca5ecb96ed71c3143ef032d1bcd559db
elmiado <sayad.mahdi75@gmail.com>
2025-09-03 16:45:35 +0330

    feat: add schemas to adapt new dynamic indicator endpoint

--- src/common/schemas.py
+++ src/common/schemas.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field
 
 from ..enums.exchange import Exchange
 from ..enums.market import Market
 from ..enums.data_type import DataType
 from ..enums.indicator_type import IndicatorType
 
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union, Annotated, Literal
 
 
+# --- Market Subscribe ---
 class MarketSubscriptionRequestSchema(BaseModel):
     exchange: Exchange
     market: Market
@@ -20,12 +21,27 @@ class MarketSubscriptionRequestSchema(BaseModel):
         return v
 
 
-class IndicatorSubscriptionRequest(BaseModel):
+# --- Indicator Subscribe ---
+class BaseIndicatorRequest(BaseModel):
     exchange: str
     market: str
     indicator: IndicatorType
 
 
+# --- RSI specific ---
+class RSISubscriptionRequest(BaseIndicatorRequest):
+    indicator: Literal[IndicatorType.RSI]
+    period: Literal[5, 10, 14] = 14
+    timeframe: Literal["1m", "5m"] = "1m"
+
+
+IndicatorSubscriptionRequest = Annotated[
+    Union[RSISubscriptionRequest],
+    Field(discriminator="indicator"),
+]
+
+
+# --- Response Schema ---
 class SubscriptionResponseSchema(BaseModel):
     channel: str
 
