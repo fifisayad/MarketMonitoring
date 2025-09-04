acf920b9a3995b73c353510090862a2063b79867
elmiado <sayad.mahdi75@gmail.com>
2025-09-04 05:50:15 +0330

    fix: reformat indicator factory method to adapt new changes

--- src/service/indicators/indicator_factory.py
+++ src/service/indicators/indicator_factory.py
@@ -2,22 +2,22 @@ from ...enums.indicator_type import IndicatorType
 from ...enums.exchange import Exchange
 from ...enums.market import Market
 from .base import BaseIndicator
-from .rsi_indicator import RSIIndicator
+from .rsi.hyperliquid_rsi_indicator import HyperLiquidRSIIndicator
 from .macd_indicator import MACDIndicator
 from .sma_indicator import SMAIndicator
 
 
-def create_indicator(
+def create_indicator_worker(
     exchange: Exchange,
     market: Market,
     data_type: IndicatorType,
     **kwargs,
 ) -> BaseIndicator:
     if data_type == IndicatorType.RSI:
-        return RSIIndicator(exchange=exchange, market=market, **kwargs)
+        if exchange == Exchange.HYPERLIQUID:
+            return HyperLiquidRSIIndicator(exchange=exchange, market=market)
     elif data_type == IndicatorType.MACD:
-        return MACDIndicator(exchange=exchange, market=market, **kwargs)
+        return MACDIndicator(exchange=exchange, market=market)
     elif data_type == IndicatorType.SMA:
-        return SMAIndicator(exchange=exchange, market=market, **kwargs)
-    else:
-        raise ValueError(f"There isn't indicator for {data_type.value}")
+        return SMAIndicator(exchange=exchange, market=market)
+    raise ValueError(f"couldnt find {data_type.value} indicator for {exchange.value}")
