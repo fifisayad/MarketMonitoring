ed81d650b2d598a24b50ad7b5a81644d22142257
fifisayad <sayad.mehrdad@gmail.com>
2025-10-13 20:03:30 +0330

    refactor: rm service

--- src/service/info/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any, Dict, List
-
-
-class BaseInfo(ABC):
-    @abstractmethod
-    def candle_snapshot(self, *args, **kwargs) -> List[Dict[Any, Any]]:
-        """retrive candle snapshots from exchange"""
-        pass
