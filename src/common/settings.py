e06c6c57cf0703a634cc78fc1e69265ad3fe1e90
fifisayad <sayad.mehrdad@gmail.com>
2025-10-13 17:02:16 +0330

    feat: add exchanges and markets as configurations

--- src/common/settings.py
+++ src/common/settings.py
@@ -1,11 +1,28 @@
+from typing import List
 from dotenv import load_dotenv
-from hyperliquid.utils.constants import MAINNET_API_URL, TESTNET_API_URL
 from pydantic_settings import BaseSettings
+from pydantic import field_validator
+from fifi.enums import Exchange, Market
 
 
 class Settings(BaseSettings):
-    HYPERLIQUID_BASE_URL: str = MAINNET_API_URL
+    def __init__(self):
+        load_dotenv()
+        super().__init__()
+
+    EXCHANGES: List[Exchange] = [Exchange.HYPERLIQUID]
+
+    @field_validator("Exchanges", mode="before")
+    @classmethod
+    def decode_exchanges(cls, v: str) -> List[Exchange]:
+        return [Exchange(x) for x in v.split(",")]
+
+    MARKETS: List[Market] = [Market.BTCUSD_PERP]
+
+    @field_validator("MARKETS", mode="before")
+    @classmethod
+    def decode_markets(cls, v: str) -> list[Market]:
+        return [Market(x) for x in v.split(",")]
+
     RESTART_TIME_THRESHOLD: float = 10
     LOG_LEVEL: str = "INFO"
-
-    model_config = {"env_file": ".env", "env_nested_delimiter": "__", "extra": "allow"}
